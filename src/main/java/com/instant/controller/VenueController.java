33f4eb56b4e5d044fd7f77a1954f631e650e6f46
Teodor Rupi <teodorrupi@gmail.com>
2016-01-29 17:53:22 +0100

    adding post request with user venue data registration

--- src/main/java/com/instant/controller/VenueController.java
+++ src/main/java/com/instant/controller/VenueController.java
@@ -5,7 +5,6 @@ import com.instant.persistence.repository.VenueRepository;
 import com.instant.validator.VenueValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -25,7 +24,7 @@ public class VenueController {
     @Autowired
     VenueRepository venueRepository;
 
-    @RequestMapping(value = Mappings.RESTAURANT_CREATE_DO, method = RequestMethod.POST)
+    @RequestMapping(value = Mappings.VENUE_SAVE, method = RequestMethod.POST)
     public ModelAndView save(Venue venue) {
         Map<String, String> errorsMap = venueValidator.isValid(venue);
         if (errorsMap.isEmpty()) {
@@ -40,20 +39,20 @@ public class VenueController {
 
     }
 
-    @RequestMapping(value = Mappings.RESTAURANT_CREATE_DO, method = RequestMethod.POST)
-    public ModelAndView saveVenue(@RequestBody Venue venue) {
-        Map<String, String> errorsMap = venueValidator.isValid(venue);
-        if (errorsMap.isEmpty()) {
-            ModelAndView view = new ModelAndView("index");
-            venueRepository.save(venue);
-            venueRepository.findByName("name");
-            return view;
-        } else {
-            ModelAndView view = new ModelAndView("index");
-            return view;
-        }
-
-    }
+//    @RequestMapping(value = Mappings.VENUE_SAVE, method = RequestMethod.POST)
+//    public ModelAndView saveVenue(@RequestBody Venue venue) {
+//        Map<String, String> errorsMap = venueValidator.isValid(venue);
+//        if (errorsMap.isEmpty()) {
+//            ModelAndView view = new ModelAndView("index");
+//            venueRepository.save(venue);
+//            venueRepository.findByName("name");
+//            return view;
+//        } else {
+//            ModelAndView view = new ModelAndView("index");
+//            return view;
+//        }
+//
+//    }
 
 
 }
