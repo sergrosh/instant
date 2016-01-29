6966b803287e026d20594f0dd668350609562dab
Sergey Roshchupkin <sergey.roshchupkin@trivago.com>
2016-01-29 17:16:23 +0100

    added ehcache

--- src/main/java/com/instant/config/ApplicationConfiguration.java
+++ src/main/java/com/instant/config/ApplicationConfiguration.java
@@ -1,8 +1,13 @@
 package com.instant.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -19,4 +24,18 @@ import org.springframework.stereotype.Controller;
 @ComponentScan(basePackages = "com.instant", excludeFilters = {@ComponentScan.Filter(Configuration.class), @ComponentScan.Filter(Controller.class)})
 @EnableMongoRepositories("com.instant.persistence.repository")
 public class ApplicationConfiguration {
+    @Bean
+    public EhCacheManagerFactoryBean  ehCacheManagerFactoryBean() {
+        EhCacheManagerFactoryBean ehCacheManagerFactoryBean = new EhCacheManagerFactoryBean();
+        ehCacheManagerFactoryBean.setConfigLocation(new ClassPathResource("ehcache.xml"));
+        return ehCacheManagerFactoryBean;
+    }
+
+    @Bean
+    @Autowired
+    public EhCacheCacheManager cacheManager(EhCacheManagerFactoryBean ehcache) {
+        EhCacheCacheManager ehCacheCacheManager = new EhCacheCacheManager();
+        ehCacheCacheManager.setCacheManager(ehcache.getObject());
+        return ehCacheCacheManager;
+    }
 }
