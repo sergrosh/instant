ab27e477f6f767cf7c0c3b7ad631d916d6db8799
Teodor Rupi <teodorrupi@gmail.com>
2016-02-03 00:57:49 +0100

    fixing user-add venue form and frontend

--- src/main/webapp/WEB-INF/originalContent/js/scripts.js
+++ src/main/webapp/WEB-INF/originalContent/js/scripts.js
@@ -1,1 +1,15 @@
 // Empty JS for your own code to be here
+
+$(function(){
+
+
+	//make text area bigger
+	var textarea_height = $('textarea.expand').height();
+    $('textarea.expand').focus(function () {
+        $(this).animate({ height: "200px" }, 500);
+    });
+    $('textarea.expand').focusout(function () {
+        $(this).animate({ height: textarea_height }, 500);
+    });
+
+});
