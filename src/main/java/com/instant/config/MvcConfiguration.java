2962b3dd932497529e1f449137e95eba3e4d35ba
Sergii Roshchupkin <sergey.roshchupkin@gmail.com>
2016-02-26 23:32:21 +0100

    added error controller, changed mvcConfiguration

--- src/main/java/com/instant/config/MvcConfiguration.java
+++ src/main/java/com/instant/config/MvcConfiguration.java
@@ -1,7 +1,10 @@
 package com.instant.config;
 
 import com.instant.common.ArgumentResolverComposite;
+import com.instant.controller.InstantErrorController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ErrorAttributes;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -22,4 +25,10 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(argumentResolverComposite);
     }
+
+    @Autowired
+    private ErrorAttributes errorAttributes;
+
+    @Bean
+    public InstantErrorController instantErrorController(){return new InstantErrorController(errorAttributes);}
 }
