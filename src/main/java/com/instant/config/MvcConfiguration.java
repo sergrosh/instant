96d2418a085584e8680c09cef480484ff50497cf
Sergey Roshchupkin <sergey.roshchupkin@gmail.com>
2016-03-03 14:02:15 +0100

    add resource handler

--- src/main/java/com/instant/config/MvcConfiguration.java
+++ src/main/java/com/instant/config/MvcConfiguration.java
@@ -23,6 +23,7 @@ import org.springframework.web.multipart.support.StandardServletMultipartResolve
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -113,6 +114,12 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter {
 
     @Autowired
     private ArgumentResolverComposite argumentResolverComposite;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/static/");
+    }
+
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
